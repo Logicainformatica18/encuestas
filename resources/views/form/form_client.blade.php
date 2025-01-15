fb70866029d58a9de77a8c6a34e8e6c8e604e41a
logicainformatica18 <logicainformatica18@gmail.com>
2025-01-15 17:52:14 -0500

    form 100%

--- resources/views/form/form_client.blade.php
+++ resources/views/form/form_client.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/jpg" href="{{ asset('ayba/favicon.png') }}" />
+    <link rel="icon" type="image/jpg" href="{{ asset('logo.png') }}" />
     <!-- Core Css -->
     <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}" />
     <title>ComexLat</title>
